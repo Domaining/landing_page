fd105fc28cf8c9c1292677502665de7805ad8f74
Rex739 <ojemehprogress@gmail.com>
2023-06-24 08:46:59 +0100

    integrated navlist to fit design

--- src/components/layout/navbar/navList.js
+++ src/components/layout/navbar/navList.js
@@ -4,33 +4,38 @@ import ActiveLink from "./activeLink"
 
 const NavList = ({ isClicked, handleClick }) => {
   return (
-    <section className="relative bg-[#0E4236] z-30 font-inter text-[32px]">
+    <section className="relative z-30 font-satoshi font-medium ">
       <nav
         className={
           isClicked
-            ? "w-full bg-green1/50 backdrop-blur-[16px] backdrop-saturate-[180%] text-white fixed h-screen top-0 left-0 pl-5"
+            ? "w-full bg-light-grey fixed h-screen top-0 left-0 pl-5"
             : "invisible"
         }
       >
         <ul
-          className={isClicked ? "flex flex-col items-start h-full" : "hidden"}
+          className={
+            isClicked ? "wrapper flex flex-col items-start h-full" : "hidden"
+          }
         >
-          <li className="flex relative py-7 mb-16">
-            <div className={isClicked ? "" : "hidden"} onClick={handleClick}>
+          <li className="flex relative py-7 mb-16 ml-auto">
+            <div
+              className={isClicked ? "ml-auto" : "hidden"}
+              onClick={handleClick}
+            >
               <img src="images/navbar/close.svg" alt="close menu" width={30} />
             </div>
           </li>
           {/* nav list */}
-          <ul className={["flex flex-col h-full"].join(" ")}>
+          <ul className={["flex flex-col text-xl"].join(" ")}>
             <div>
               <ActiveLink
-                className="mb-5"
+                className="mb-10"
                 href="/"
                 handleClick={handleClick}
                 children={"Home"}
               ></ActiveLink>
               <ActiveLink
-                className="mb-5"
+                className="mb-10"
                 href="/#about"
                 handleClick={handleClick}
                 children={"About"}
@@ -43,6 +48,18 @@ const NavList = ({ isClicked, handleClick }) => {
               ></ActiveLink>
             </div>
           </ul>
+          <li className="mt-20">
+            <a href="">
+              <button
+                className={[
+                  "underlineFromLeft", // css code in global.css
+                  "font-bold bg-blue1 w-[170px] h-[50px] text-white rounded-md",
+                ].join(" ")}
+              >
+                Launch App
+              </button>
+            </a>
+          </li>
         </ul>
       </nav>
     </section>
