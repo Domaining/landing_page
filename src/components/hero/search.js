82ff55885e7bc8796cf8b5cfa9246f07365029b4
Rex739 <ojemehprogress@gmail.com>
2023-06-24 07:47:13 +0100

    integrated search bar

---
+++ src/components/hero/search.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+const Search = () => {
+  return (
+    <section>
+      {/* <div className="flex flex-col xss:flex-row w-full  justify-center font-extrabold mb-10">
+        <a href="">
+          {" "}
+          <button
+            className={[
+              "w-full h-[40px] m-0 mb-3 xss:mb-0 xss:w-[130px]",
+              "xss:h-[40px] lg:w-[312px] lg:h-[60px]",
+              "rounded-lg bg-orange xss:mr-[9px] underlineFromLeft",
+            ].join(" ")}
+          >
+            Go To App
+          </button>
+        </a>
+
+        <a href="">
+          {" "}
+          <button
+            className={[
+              "w-full h-[40px] m-0 mb-3 xss:mb-0 border-[1.5px]",
+              "xss:w-[130px] xss:h-[40px] lg:w-[312px] lg:h-[60px]",
+              "rounded-lg bg-tranparent border-white1 xss:ml-[9px]",
+            ].join(" ")}
+          >
+            Learn
+          </button>
+        </a>
+      </div> */}
+    
+    </section>
+  )
+}
+ 
+export default Search;
