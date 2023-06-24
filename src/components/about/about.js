8d5cf9fe8141c7b8c9665d13933abe56e6164cd4
Rex739 <ojemehprogress@gmail.com>
2023-06-24 15:35:46 +0100

    implemented about us

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -6,15 +6,15 @@ import { aboutData } from "./data"
 
 const About = () => {
   return (
-    <section className="wrapper font-inter text-lg lg:text-xl text-white1 text-left lg:text-center"
-    id="about">
+    <section className="wrapper text-left" id="about">
       {aboutData.map((data) => (
         <div key={data.id} className="py-10 lg:py-20">
           <SectionHeader
             sectionTitle={data.sectionTitle}
-            subTitle={data.subTitle}
+           
           />
           <SectionBody
+            subTitle={data.subTitle}
             paragraph1={data.paragraph1}
             paragraph2={data.paragraph2}
             sectionImage={data.sectionImage}
