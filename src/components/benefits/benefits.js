bce9993f5c5db9b8b14a256e88a42b6238005996
Rex739 <ojemehprogress@gmail.com>
2023-06-25 13:07:54 +0100

    integrated benefits section

---
+++ src/components/benefits/benefits.js
@@ -0,0 +1,38 @@
+import React from "react"
+import SectionBody from "../sectionBody"
+import SectionHeader from "../sectionHeader"
+import { benefitsData } from "./data"
+
+const Benefits = () => {
+  return (
+    <section className="text-left">
+      {benefitsData.map((data) => (
+        // @ts-ignore
+        <div key={data.id}>
+          <div className="bg-light-grey py-10 lg:py-20">
+            {" "}
+            <section className="wrapper">
+              <SectionHeader
+                sectionTitle={data.sectionTitle}
+                subTitle={undefined} 
+              />
+              <SectionBody
+                paragraph1={data.paragraph1}
+                paragraph2={data.paragraph2}
+                paragraph3={data.paragraph3}
+                paragraph4={data.paragraph4}
+                sectionImage={data.sectionImage}
+                sectionName={data.sectionName}
+                flexRow={undefined}
+                flexRowReverse={data.flexRowReverse}
+                subTitle={undefined}
+              />
+            </section>
+          </div>
+        </div>
+      ))}
+    </section>
+  )
+}
+
+export default Benefits
---
+++ src/components/benefits/data.js
@@ -0,0 +1,41 @@
+import React from "react"
+
+export const benefitsData = [
+  {
+    id: 0,
+    sectionTitle: "Benefits",
+    // subTitle: "We provide the best solution to existing problems",
+    paragraph1: (
+      <p className="mb-5">
+        <span className="font-satoshiBold">Interoperability - </span>
+        Improved decentralized, secure and non-custodial wallet that allow users
+        to pass crypto as inheritance to their loved ones.
+      </p>
+    ),
+    paragraph2: (
+      <p className="mb-5">
+        <span className="font-satoshiBold">Security - </span>
+        Your data is stored on the blockchain, where it is secure and
+        tamper-proof.
+      </p>
+    ),
+    paragraph3: (
+      <p className="mb-5">
+        <span className="font-satoshiBold">Scalability - </span>
+        designed to meet the needs of a growing user base.
+      </p>
+    ),
+    paragraph4: (
+      <p className="">
+        <span className="font-satoshiBold">Global Accessibility - </span>
+        NAMEE3 domains are accessible worldwide, transcending geographical
+        boundaries and ensuring your digital presence can be accessed by anyone
+        with an internet connection, fostering global reach and engagement.
+      </p>
+    ),
+    sectionImage: "images/benefits/benefits.svg",
+    sectionName: "Benefits",
+    flexRow: "lg:flex-row",
+    flexRowReverse: "lg:flex-row-reverse",
+  },
+]
