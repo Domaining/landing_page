9c3de50f9d68eb4df2b1445c297679a39c4bbdad
Olotu Oluwagbemiga <gbenga.isess@gmail.com>
2023-06-26 10:43:01 +0100

    Modification to the next.config file

--- next.config.js
+++ next.config.js
@@ -1,3 +1,20 @@
 module.exports = {
-  reactStrictMode: true,
-}
+  webpack: (config) => {
+    config.module.rules.push(
+      {
+        test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 8192,
+            publicPath: '/_next',
+            outputPath: 'static',
+            name: '[name].[hash].[ext]',
+          },
+        },
+      }
+    );
+
+    return config;
+  },
+};
